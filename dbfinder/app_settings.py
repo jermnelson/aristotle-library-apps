fb870893d5ab7f4898473c17562793513c3c7e46
Jeremy Nelson <jermnelson@gmail.com>
2012-09-28 17:08:28 -0600

    Still working on single-app view for fedora batch and added app settings file to dbfinder app

---
+++ dbfinder/app_settings.py
@@ -0,0 +1,16 @@
+"""
+ mod:`app_settings` Fedora Batch App Settings
+"""
+import sys,os
+import aristotle.settings as settings
+
+
+APP = {'current_view': {'title':'Database Finder'},
+       'description': 'The Database Finder App displays a listing of databases owned by the library.',
+       'icon_url':'fedora-commons.png',
+       'productivity':True,
+       'url':'dbfinder/'}
+
+
+
+
