81bca08e72d402ca880381a5a33e347a46fa569b
Jeremy Nelson <jermnelson.gmail.com>
2014-09-08 11:44:48 -0600

    Update to Springer record load

--- marc_batch/jobs/springer.py
+++ marc_batch/jobs/springer.py
@@ -7,6 +7,7 @@ __author__ = 'Jeremy Nelson'
 from marc_batch.marc_helpers import MARCModifier
 import urlparse
 from pymarc import Field
+import re
 
 PROXY_LOCATION = 'http://0-www.springerlink.com.tiger.coloradocollege.edu/openurl.asp?genre=book&id=doi:'
 
@@ -33,7 +34,7 @@ class SpringerEBookJob(MARCModifier):
         else:
             self.spr_proxy =  PROXY_LOCATION
         if kwargs.has_key('public_note'):
-            self.public_note = kwargs.get('public_note') 
+            self.public_note = kwargs.get('public_note')
         else:
             self.public_note = 'View online'
         if kwargs.has_key('note_prefix'):
@@ -42,7 +43,7 @@ class SpringerEBookJob(MARCModifier):
             self.note_prefix='Available via Internet'
 
     def processLeader(self,marc_leader):
-        ''' 
+        '''
         Method validates/sets leader positions for Spring MARC record.
 
         :param marc_leader: MARC file leader
@@ -55,45 +56,47 @@ class SpringerEBookJob(MARCModifier):
             marc_leader = marc_leader[0:16] + 'u' + marc_leader[17:]
         return marc_leader
 
-    def processRecord(self,marc_record): 
-        ''' 
-        Callback method called by base class load method, Springer 
-        eBook specific validation. 
- 
-        :param marc_record: MARC record 
-        ''' 
-        marc_record.leader = self.processLeader(marc_record.leader) 
-        marc_record = self.validate001(marc_record) 
-        marc_record = self.validate006(marc_record) 
-        marc_record = self.validate008(marc_record)         
-        marc_record = self.processSpringerURLs(marc_record)         
-        return marc_record 
- 
-    def validate001(self,marc_record): 
-        ''' Method sets 001 Control Number of CC's format. 
- 
-         :param marc_record: MARC record 
-            args: 
-            marc_record -- MARC record 
-        ''' 
-        field001 = marc_record.get_fields('001')[0] 
-        marc_record.remove_field(field001) 
-        raw_data = field001.data 
-        if raw_data.find('spr') < 0: 
-            new_data = "spr%s" % raw_data.replace("-","") 
-        else: 
-            new_data = raw_data.replace("-","") 
-        field001.data = new_data 
-        marc_record.add_field(field001) 
+    def processRecord(self,marc_record):
+        '''
+        Callback method called by base class load method, Springer
+        eBook specific validation.
+
+        :param marc_record: MARC record
+        '''
+        marc_record.leader = self.processLeader(marc_record.leader)
+        marc_record = self.validate001(marc_record)
+        marc_record = self.validate006(marc_record)
+        marc_record = self.validate008(marc_record)
+        marc_record = self.validate245(marc_record)
+        marc_record = self.validate300(marc_record)
+        marc_record = self.processSpringerURLs(marc_record)
+        return marc_record
+
+    def validate001(self,marc_record):
+        ''' Method sets 001 Control Number of CC's format.
+
+         :param marc_record: MARC record
+            args:
+            marc_record -- MARC record
+        '''
+        field001 = marc_record.get_fields('001')[0]
+        marc_record.remove_field(field001)
+        raw_data = field001.data
+        if raw_data.find('spr') < 0:
+            new_data = "spr%s" % raw_data.replace("-","")
+        else:
+            new_data = raw_data.replace("-","")
+        field001.data = new_data
+        marc_record.add_field(field001)
         return marc_record
 
 
     def validate006(self,marc_record):
-        ''' 
+        '''
         Method checks/sets 006 fixed length data elements in MARC
         record.
 
-        :param marc_record: MARC record 
+        :param marc_record: MARC record
         '''
         existing_fields = marc_record.get_fields('006')
         if existing_fields:
@@ -101,17 +104,17 @@ class SpringerEBookJob(MARCModifier):
             marc_record.remove_field(field006)
         else:
             field006 = Field(tag='006',indicators=None)
-        field006.data = r'm        d        '
+        field006.data = r'm    o   d        '
         marc_record.add_field(field006)
         return marc_record
 
 
     def validate008(self,marc_record):
-        ''' 
+        '''
         Method checks/sets 008 fixed length data elements in MARC
         record.
 
-        :param marc_record: MARC record 
+        :param marc_record: MARC record
         '''
         field008 = marc_record.get_fields('008')[0]
         marc_record.remove_field(field008)
@@ -124,13 +127,58 @@ class SpringerEBookJob(MARCModifier):
         marc_record.add_field(field008)
         return marc_record
 
+    def validate245(self, marc_record):
+        marc_record = super(SpringerEBookJob, self).validate245(marc_record)
+        all245s = marc_record.get_fields('245')
+        for field245 in all245s:
+            field245.delete_subfield('h')
+        return marc_record
+
+    def validate300(self, marc_record):
+        """Method modifies existing 300 field to the following RDA format:
+
+        300  1 online resource (xxvi, 368 pages) : $b illustrations
+
+        Args:
+            marc_record(pymarc.Record): Input MARC21
+
+        Returns:
+            pymarc.Record: Modified MARC21
+        """
+        preface_pages_re = re.compile(r"(\w+), (\w+) p+")
+        illus_re = re.compile(r"illus")
+        all300Fields = marc_record.get_fields('300')
+        for field in all300Fields:
+            new_a = "online resource"
+            new300 = Field(tag='300', indicators=['1',' '])
+            subfield_a_lst = field.get_subfields('a')
+            if len(subfield_a_lst) < 1:
+                new300.add_subfield('a', new_a)
+            for subfield in subfield_a_lst:
+                illus_search = illus_re.search(subfield)
+                preface_search = preface_pages_re.search(subfield)
+
+                if preface_search is not None:
+                    preface, pages = preface_search.groups()
+                    new_a = "{} ({}, {} pages)".format(
+                        new_a,
+                        preface.lower(),
+                        pages)
+                    if illus_search is not None:
+                        new_a += " :"
+                new300.add_subfield('a', new_a)
+                if illus_search is not None:
+                    new300.add_subfield('b', 'illustrations')
+            marc_record.remove_field(field)
+            marc_record.add_field(new300)
+        return marc_record
 
     def processSpringerURLs(self,marc_record):
         '''
         Method overrides parent processURLS for Springer specific
         modification of the 538 and 856 fields
 
-        :param marc_record: MARC record 
+        :param marc_record: MARC record
          '''
         all856fields = marc_record.get_fields('856')
         field856 = all856fields[0]
